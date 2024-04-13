0695d90d5904e0daed667f0f272c7c4b0eddba7e
ulas312 <temel.ulas@googlemail.com>
2024-04-13 01:18:51 +0100

    working on search bar

---
+++ full-stacked-v2.0/src/components/SearchExercises.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+
+function SearchExercises() {
+  return (
+    <Stack alignItems='center' mt='37px' justifyContent='center' p='20px'>
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: '44px', xs: '30px' } }}
+        mb='50px'
+        textAlign='centre'
+      >
+        Search For An Exercise
+      </Typography>
+      <Box position='relative' mb='72px'>
+        <TextField
+          sx={{
+            input: { fontWeight: '700', border: 'none', borderRadius: '4px' },
+            width: { lg: '1170px', sx: '350px' },
+            backgroundColor: '#fff',
+            borderRadius: '40px',
+          }}
+          height='76px'
+          value=''
+          onChange={(e) => {}}
+          placeholder='Search  Exercises'
+          type='text'
+        />
+        <Button
+          className='search-btn'
+          sx={{
+            bgcolor: '#ff2625',
+            color: '#fff',
+            textTransform: 'none',
+            width: { lg: '175px', xs: '80px' },
+            fontSize: { lg: '20px', sx: '14px' },
+            height: '56px',
+            position: 'absolute',
+          }}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
+  );
+}
+
+export default SearchExercises;
