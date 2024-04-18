f7fdb767724f4faffc1d488f5a27df17e7b2c914
ulas312 <temel.ulas@googlemail.com>
2024-04-19 00:13:24 +0100

    scroll bar ready for exercise component

--- full-stacked-v2.0/src/components/BodyPart.js
+++ full-stacked-v2.0/src/components/BodyPart.js
@@ -11,25 +11,33 @@ export const BodyPart = ({ item, setBodyPart, bodyPart }) => {
       alignItems='center'
       justifyContent='center'
       className='bodyPart-card'
-      sx={
-        bodyPart === item
-          ? {
-              borderTop: '4px solid #ff2625',
-              borderBottomLeftRadius: '20px',
-              backgroundColor: '#fff',
-              width: '270px',
-              height: '270px',
-              cursor: 'pointer',
-              gap: '47px',
-            }
-          : ''
-      }
+      sx={{
+        borderTop: bodyPart === item ? '4px solid #ff2625' : '',
+        borderBottomLeftRadius: '20px',
+        backgroundColor: '#fff',
+        width: '270px',
+        height: '270px',
+        cursor: 'pointer',
+        gap: '47px',
+      }}
+      onClick={() => {
+        setBodyPart(item);
+        window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
+      }}
     >
       <img
         src={Icon}
         alt='Dumbbell Icon'
-        styles={{ width: '40px', height: '40px' }}
+        style={{ width: '40px', height: '40px' }}
       />
+      <Typography
+        fontSize='24px'
+        fontWeight='bold'
+        color='#3A1212'
+        textTransform='capitalize'
+      >
+        {item}
+      </Typography>
     </Stack>
   );
 };
